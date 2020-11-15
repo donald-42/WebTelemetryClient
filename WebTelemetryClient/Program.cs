32d3972e712a24d9a1ff3827903a2248d018c25c
donald-42 <heiko.wulf@udo.edu>
2020-11-15 18:56:37 +0100

    Add project files.

---
+++ WebTelemetryClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebTelemetryClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WebTelemetryClient/f1receiver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+namespace WebTelemetryClient
+{
+    /// <summary>
+    /// Class to receive all the Telemetry Data send by the game. Will also store the Data received until program closes. -- asynchronously, of course
+    /// </summary>
+    class f1receiver
+    {
+
+
+    }
+}
